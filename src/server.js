f96fbd841e54635fc03a43b3f337d4e5c7c6b7e6
maeek <maciek299.ms@gmail.com>
2019-05-04 19:06:01 +0200

    Fix

---
+++ src/server.js
@@ -0,0 +1,60 @@
+/*
+ *   Author: maeek
+ *   Description: No history simple websocket chat
+ *   Github: https://github.com/maeek/vv-chat
+ *   Version: 1.1.0
+ * 
+ */
+
+
+const fs = require('fs');
+const config = require('./config');
+
+/* Load express app */
+const app = require('./expressApp');
+
+/* 
+ * Checking if certificate files exist 
+ */
+let certFlag = true,
+    certNotFound = [],
+    server;
+for (const file in config.certificateFiles) {
+    if (!fs.existsSync(config.certificateFiles[file])) {
+        certNotFound.push(config.certificateFiles[file])
+        certFlag = false;
+    }
+}
+
+if (certFlag) {
+    /* 
+     * Certificates exist, starting HTTPS 
+     */
+    const privateKey = fs.readFileSync(config.certificateFiles.priv, 'utf8');
+    const certificate = fs.readFileSync(config.certificateFiles.cert, 'utf8');
+    const ca = fs.readFileSync(config.certificateFiles.ca, 'utf8');
+    const options = {
+        protocols: ['h2', 'spdy/3.1', 'spdy/3', 'spdy/2'],
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+
+    server = require('spdy').createServer(options, app);
+    config.https = true;
+} else {
+    /* 
+     * No certificates, starting HTTP, NOT SECURE!
+     */
+    console.log(`Couldn't find this certificates:\n${certNotFound.join("\n")}`);
+    console.log(`###################################\n#     SERVER RUNNING PLAIN HTTP   #\n###################################`);
+    server = require('http').createServer(app);
+    config.https = false;
+}
+
+/* Load socket.io */
+const io = require('socket.io')(server);
+/* Load sockets functions */
+require('./sockets')(io);
+
+module.exports = server;
